66d007bcbb1d3009580c15dab469b3c9e5107c77
AngelicaBarcelona08 <59691341+AngelicaBarcelona08@users.noreply.github.com>
2020-01-10 17:51:18 +0800

    Add files via upload

---
+++ MidtermAct03.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class MidtermAct03
+{
+	public static void main(String[] args){
+		try {
+			File Record = new File ("Phonebook.txt");
+			Scanner scanner = new Scanner (Record);
+			String code =" ";
+			while(scanner.hasNext()) {
+				code = scanner.next();
+				if(code.equals("'1001-11")) {
+					System.out.println("The code found");
+					System.out.println("Code:" +code);
+					System.out.println("First Name" + scanner.nextLine());
+					System.out.println("Last Name" + scanner.nextLine());
+					System.out.println("Cp No." + scanner.nextInt());
+					continue;
+				}
+			}
+			scanner.close();
+			}
+			catch(IOException e) {
+				System.out.println();
+			}
+		}		
+	}
+		
